23a21de25483de576bd69d0f927e694c9de05562
shichao-timeout <sc15052109526@163.com>
2022-08-24 21:25:44 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/howlinuxworks.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/howlinuxworks.dir/main.c.obj"
+  "howlinuxworks.exe"
+  "howlinuxworks.exe.manifest"
+  "howlinuxworks.pdb"
+  "libhowlinuxworks.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/howlinuxworks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
